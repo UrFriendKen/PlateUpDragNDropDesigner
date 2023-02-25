00bdab780e2eee611a7d7990cd771444761eaa87
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-02-25 20:08:52 +0800

    Bump version number

--- Main.cs
+++ Main.cs
@@ -12,7 +12,7 @@ namespace KitchenDragNDropDesigner
     {
         public const string MOD_GUID = "IcedMilo.PlateUp.DragNDropDesigner";
         public const string MOD_NAME = "Drag N' Drop Designer";
-        public const string MOD_VERSION = "0.1.5";
+        public const string MOD_VERSION = "0.1.6";
         public const string MOD_AUTHOR = "IcedMilo";
         public const string MOD_GAMEVERSION = ">=1.1.4";
 
